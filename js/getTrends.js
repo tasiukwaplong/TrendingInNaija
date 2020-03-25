c28bfbe9166d2a703c1025ff89096db8f347a271
Tasiu Kwaplong <tasiukwaplong@gmail.com>
2020-03-24 22:04:01 -0700

    Initial commit

---
+++ js/getTrends.js
@@ -0,0 +1,30 @@
+let allTrends = "hello"
+var getTrends = new Vue({  
+  el: '#trends',
+  data () {
+    return {
+    	API_URL: 'http://localhost:5000/api/v1/trends',
+    	trends: null,
+    	loading: true,
+    	errored: false,
+    }
+  },
+  mounted () {
+    axios
+      .get(this.API_URL)
+      .then(response => {
+      	// console.log(allTrends)
+        this.trends = response.data.data
+        updateAllTrends(response.data.data)
+        // this.errored =  response.data.data.errorExist
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
+  }
+});
+function updateAllTrends(data) {
+  allTrends = data
+}
