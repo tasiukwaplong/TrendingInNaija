cc59b6284e75ff0b06f6364f62ef46f45049a143
Tasiu Kwaplong <tasiukwaplong@gmail.com>
2020-03-26 20:57:08 -0700

    Users count works

--- js/getTrends.js
+++ js/getTrends.js
@@ -25,6 +25,32 @@ var getTrends = new Vue({
       .finally(() => this.loading = false)
   }
 });
+
+
+var getUsers = new Vue({  
+  el: '#users',
+  data () {
+    return {
+      API_URL: 'http://localhost:5000/api/v1/getuserlength',
+      users: null
+    }
+  },
+  mounted () {
+    axios
+      .get(this.API_URL)
+      .then(response => {
+        // console.log(response.data.status)
+        this.users = response.data.status
+        // updateAllTrends(response.data.data)
+        // this.errored =  response.data.data.errorExist
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
+  }
+});
 function updateAllTrends(data) {
   allTrends = data
 }
